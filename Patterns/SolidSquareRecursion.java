bb170d0addc04d1330cd1e538cf5dfad06e0954e
Ansh Ruhela <ruhelayash26@gmail.com>
2024-10-17 18:32:22 +0530

    Solid Square Pattern Using Recursion

--- Patterns/SolidSquareRecursion.java
+++ Patterns/SolidSquareRecursion.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 public class SolidSquareRecursion
 {
-	public static void main(String[] args)
+	static void printPattern(int n) {
+		row(0, n);
+	}
+
+	private static void row(int i, int n)
 	{
-		System.out.println("Hello World");
+		if(i<n)
+		{
+			col(0, n);
+			System.out.println();
+			row(i+1,n);
+		}
+		return;
+	}
+
+	private static void col(int j, int n) {
+		if (j < n) {
+			System.out.print(" * ");
+			col(j + 1, n);
+		}
+		return;
+	}
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.print("Enter the value of n(number of rows) : ");
+		n = new java.util.Scanner(System.in).nextInt();
+		printPattern(n);
 	}
 }
